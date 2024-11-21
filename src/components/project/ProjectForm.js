9c3d598690c4a34097b8e4851737c59fbecfe357
Marcos Apolo <marcosapolo511@gmail.com>
2024-11-21 19:20:25 -0300

    react

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -10,17 +10,17 @@ function ProjectForm({ btnText }) {
 
     const [categories, setCategories] = useState([])
 
-    useEffect(()=>{
-        fetch ( "http://localhost:5000/categories", {
-            method: "GET",
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-        
-        }).then((res)=>res.json())
-        .then((data)=>{setCategories(data)})
-    })
-    
+    fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+    }).then((resp) => resp.json())
+        .then((data) => { setCategories(data) })
+        .catch((err) => console.log(err))
+
+
 
     return (
         <form className={style.form}>
